ed97a54cf1c7291c1c96fde7eace4f4da496456b
Sunyoung07 <61489829+Sunyoung07@users.noreply.github.com>
2020-11-23 03:31:32 +0900

    임시 문제은행 페이지

---
+++ mobileappproject/lib/bank.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+import 'dart:math';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:async';
+
+
+class BankPage extends StatefulWidget {
+  @override
+  _BankPageState createState() => _BankPageState();
+}
+
+class _BankPageState extends State<BankPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Bank'),
+        ),
+        body: Container(
+            margin: EdgeInsets.fromLTRB(85, 30, 0, 10),
+            child: Text('여러 사용자의 문제들 보여주는 페이지')
+        )
+    );
+  }
+}
