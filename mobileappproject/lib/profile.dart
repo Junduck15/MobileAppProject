3c77d8d4d0b84e4dc2eb8a3cc8c44a1a59fa2888
Sunyoung07 <61489829+Sunyoung07@users.noreply.github.com>
2020-11-29 22:54:16 +0900

    으캬캬

--- mobileappproject/lib/profile.dart
+++ mobileappproject/lib/profile.dart
@@ -8,46 +8,45 @@ class Profile extends StatelessWidget {
   Profile({
     Key key,
   }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     String temp =
         auth.currentUser.email == null ? 'Anonymous' : auth.currentUser.email;
     String image = auth.currentUser.photoURL == null
-        ? 'http://handong.edu/site/handong/res/img/logo.png'
+        ? 'https://ifh.cc/g/PZ4pEO.png'
         : auth.currentUser.photoURL;
-    return MaterialApp(
-      title: 'Flutter layout demo',
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
-          title: Text('Main'),
-          actions: <Widget>[
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Page', style: TextStyle(color: Colors.white)),
+        actions: <Widget>[
+          Column(children: <Widget>[
             IconButton(
-                icon: Icon(Icons.exit_to_app),
+                icon: Icon(
+                  Icons.exit_to_app,
+                  color: Colors.white,
+                ),
                 onPressed: () {
                   auth.signOut();
                   Navigator.push(context,
                       MaterialPageRoute(builder: (context) => SignInPage()));
                 }),
-          ],
-        ),
-        body: ListView(
-          children: <Widget>[
-            Image.network(
-              image,
-              width: 800,
-            ),
-            Text(auth.currentUser.uid, style: TextStyle(color: Colors.white)),
-            Text(temp, style: TextStyle(color: Colors.white)),
-          ],
-        ),
+          ])
+        ],
+      ),
+      body: ListView(
+        children: <Widget>[
+          Image.network(
+            image,
+            width: 800,
+          ),
+          Text(
+            auth.currentUser.uid,
+          ),
+          Text(
+            temp,
+          ),
+        ],
       ),
     );
   }
