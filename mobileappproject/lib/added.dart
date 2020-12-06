494107fed614bf403a5cd5640e4412e83f0ed2b9
Sunyoung07 <61489829+Sunyoung07@users.noreply.github.com>
2020-12-06 18:48:36 +0900

    수정 기능 삭제

--- mobileappproject/lib/added.dart
+++ mobileappproject/lib/added.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'home.dart';
-import 'edit.dart';
 import 'package:mobileappproject/login.dart';
 
 class Added extends StatelessWidget {
@@ -10,13 +9,21 @@ class Added extends StatelessWidget {
   final bool isMul;
   final AsyncSnapshot snap;
   final String problemType;
-  const Added({Key key, this.problem, this.answer, this.isMul, this.snap, this.problemType}) : super(key: key);
+
+  const Added(
+      {Key key,
+      this.problem,
+      this.answer,
+      this.isMul,
+      this.snap,
+      this.problemType})
+      : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     Widget prob = Container(
-      margin: EdgeInsets.fromLTRB(15, 10, 0, 15),
-      child: Column(
-        children: [
+        margin: EdgeInsets.fromLTRB(15, 10, 0, 15),
+        child: Column(children: [
           Center(
             child: Container(
                 decoration: BoxDecoration(
@@ -32,8 +39,8 @@ class Added extends StatelessWidget {
             height: 1.0,
           ),
         ]));
-        Widget answ = Container(
-               margin: EdgeInsets.fromLTRB(15, 10, 0, 15),
+    Widget answ = Container(
+      margin: EdgeInsets.fromLTRB(15, 10, 0, 15),
       child: Column(
         children: [
           Center(
@@ -58,28 +65,6 @@ class Added extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Container(
-            margin: EdgeInsets.only(right: 10),
-            child: RaisedButton(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              padding: EdgeInsets.fromLTRB(40, 5, 40, 5),
-              color: Colors.blue,
-              child: Text(
-                '문제 수정',
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => Edit(isMul : isMul, snap : snap, problemType: problemType),
-                    ));
-              },
-            )),
-        Container(
             margin: EdgeInsets.only(left: 10),
             child: RaisedButton(
               shape: RoundedRectangleBorder(
@@ -95,10 +80,10 @@ class Added extends StatelessWidget {
               ),
               onPressed: () {
                 Navigator.popUntil(
-                    context,
-                    ModalRoute.withName('/home'),
+                  context,
+                  ModalRoute.withName('/home'),
                 );
-               //Navigator.pop(context);
+                //Navigator.pop(context);
               },
             ))
       ],
@@ -113,18 +98,6 @@ class Added extends StatelessWidget {
             '등록된 문제 확인',
             style: TextStyle(color: Colors.white),
           ),
-            actions: <Widget>[
-              IconButton(
-            icon: Icon(Icons.edit),
-            onPressed: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => Edit(isMul : isMul),
-                  ));
-            },
-
-          )],
         ),
         body: ListView(
           children: <Widget>[
@@ -133,9 +106,8 @@ class Added extends StatelessWidget {
                 child: Text('등록된 문제')),
             prob,
             Container(
-                margin: EdgeInsets.fromLTRB(15, 10, 0, 15),
-                child: Text('정답')),
-                answ,
+                margin: EdgeInsets.fromLTRB(15, 10, 0, 15), child: Text('정답')),
+            answ,
             buttons
           ],
         ),
