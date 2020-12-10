ba97d83f80409bc1d35ed71ccce18222468b771f
Junseok <wnstjr2gkgk@gmail.com>
2020-12-10 10:08:13 +0900

    Modified Add

--- mobileappproject/lib/add.dart
+++ mobileappproject/lib/add.dart
@@ -61,6 +61,7 @@ class _Add extends State<Add> {
       try {
         final ref = FirebaseStorage.instance.ref().child(text);
         imageString = await ref.getDownloadURL();
+        print(imageString);
       } catch (e) {
         print(e.message);
         showDialog(
