488ba54140b7b80dd57b8966932602aaab159262
Yeonwoo Lim <1213terry@naver.com>
2020-12-10 14:50:11 +0900

    notification class 생성, notification setting 설정, timezone 설정
    notification class 생성, notification setting 설정, timezone 설정

--- mobileappproject/lib/main.dart
+++ mobileappproject/lib/main.dart
@@ -1,15 +1,92 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:rxdart/subjects.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
 
 import 'home.dart';
 import 'login.dart';
 import 'add.dart';
 
-void main() {
-  runApp(App());
+FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+final BehaviorSubject<ReceivedNotification> didReceiveLocalNotificationSubject =
+BehaviorSubject<ReceivedNotification>();
+
+final BehaviorSubject<String> selectNotificationSubject =
+BehaviorSubject<String>();
+
+class ReceivedNotification {
+  ReceivedNotification({
+    @required this.id,
+    @required this.title,
+    @required this.body,
+    @required this.payload,
+  });
+
+  final int id;
+  final String title;
+  final String body;
+  final String payload;
+}
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await _configureLocalTimeZone();
+
+  final NotificationAppLaunchDetails notificationAppLaunchDetails =
+  await flutterLocalNotificationsPlugin.getNotificationAppLaunchDetails();
+
+  const AndroidInitializationSettings initializationSettingsAndroid =
+  AndroidInitializationSettings('app_icon');
+  final IOSInitializationSettings initializationSettingsIOS =
+  IOSInitializationSettings(
+    requestSoundPermission: false,
+    requestBadgePermission: false,
+    requestAlertPermission: false,
+    onDidReceiveLocalNotification: (int id, String title, String body, String payload) async {
+      didReceiveLocalNotificationSubject.add(ReceivedNotification(
+          id: id, title: title, body: body, payload: payload));
+    });
+  final MacOSInitializationSettings initializationSettingsMacOS =
+  MacOSInitializationSettings(
+      requestAlertPermission: false,
+      requestBadgePermission: false,
+      requestSoundPermission: false);
+  final InitializationSettings initializationSettings = InitializationSettings(
+      android: initializationSettingsAndroid,
+      iOS: initializationSettingsIOS,
+      macOS: initializationSettingsMacOS);
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+      onSelectNotification: (String payload) async {
+        if (payload != null) {
+          debugPrint('notification payload: $payload');
+        }
+        selectNotificationSubject.add(payload);
+      });
+
+  runApp(App(notificationAppLaunchDetails,));
+}
+
+Future<void> _configureLocalTimeZone() async {
+  tz.initializeTimeZones();
+  // final String timeZoneName = await platform.invokeMethod('getTimeZoneName');
+  tz.setLocalLocation(tz.getLocation('Asia/Seoul'));
 }
 
 class App extends StatefulWidget {
+  const App(
+      this.notificationAppLaunchDetails, {
+        Key key,
+      }) : super(key: key);
+
+  final NotificationAppLaunchDetails notificationAppLaunchDetails;
+  bool get didNotificationLaunchApp =>
+      notificationAppLaunchDetails?.didNotificationLaunchApp ?? false;
   _AppState createState() => _AppState();
 }
 
@@ -17,6 +94,7 @@ class _AppState extends State<App> {
   // Set default `_initialized` and `_error` state to false
   bool _initialized = false;
   bool _error = false;
+
 
   // Define an async function to initialize FlutterFire
   void initializeFlutterFire() async {
@@ -39,6 +117,7 @@ class _AppState extends State<App> {
     initializeFlutterFire();
     super.initState();
   }
+
 
   @override
   Widget build(BuildContext context) {
